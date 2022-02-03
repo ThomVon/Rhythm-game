fa807778901f5f09ed56dcab6681e81502143068
CapDei <firestarcat75@gmail.com>
2022-02-03 15:47:33 +0200

    Menu struggles

---
+++ Rhythm game/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton() {
+        Application.Quit();      // Quits the game
+    }
+    // In the levels it starts the game
+    public void StartGame() {
+        //
+    }
+}
