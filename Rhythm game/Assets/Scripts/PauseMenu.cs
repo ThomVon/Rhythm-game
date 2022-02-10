6346df1d3503ae70df810da9f5091434e19b0c43
ThomVon <78027203+ThomVon@users.noreply.github.com>
2022-02-10 21:20:44 +0200

    yritin mutta en onnistunut

--- Rhythm game/Assets/Scripts/PauseMenu.cs
+++ Rhythm game/Assets/Scripts/PauseMenu.cs
@@ -7,17 +7,20 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;     // This should be SerializedField but there comes error if it's used
                                      // so please don't touch this gameobject in the editor
-                        
+     
+    
     public void Pause() {
         // Pauses the game
         pauseMenu.SetActive(true);
         Time.timeScale = 0f;
+        
     }
 
     public void Resume() {
         // Resumes the game
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
+        
     }
 
     public void Home(int sceneID) {
